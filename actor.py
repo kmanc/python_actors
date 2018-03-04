2b310189d61910987d0924583eb4f9998a67ffa4
Kevin <kmanc@comcast.net>
2018-03-03 16:18:59 -0800

    there are a few methods in these that need review. will go over on monday

--- actor.py
+++ actor.py
@@ -1,47 +1,68 @@
+import abc
+import concurrent.futures
 import micro_kernel_config
 import queue
 
 
-class Actor(object):
+class Actor(concurrent.futures.Future):
 
     def __init__(self):
+        self.config = micro_kernel_config.MicroKernelConfig()
+
         self.is_running = True
         self.is_complete = False
         self.message_queue = queue.Queue()
         self.actor_lookup = dict()
-        self.timer = 'timer'
 
-        self.config = micro_kernel_config.MicroKernelConfig()
-
+    @abc.abstractclassmethod
     def on_receive(self, message):
-        print('on receive')
+        pass
 
+    @abc.abstractclassmethod
     def get_name(self):
-        print('get name')
+        pass
 
     def on_init(self):
-        print('on init')
+        pass
 
     def on_complete(self):
-        print('on complete')
+        pass
 
     def on_shutdown(self):
-        print('on shutdown')
+        pass
 
+    #@Override
     def call(self):
-        print('call')
+        while self.is_running and not self.is_complete:
+            try:
+                message = self.message_queue.get_nowait()
+            except queue.Empty:
+                message = None
+            if message:
+                size = self.message_queue.qsize()
+                half = self.config.ACTOR_MESSAGE_QUEUE_SIZE >> 1
+                if size == 0:
+                    self.on_receive(message)
+                elif size > half:
+                    array = list()
+                    for i in range(half):
+                        array.append(self.message_queue.get_nowait())
+                    for stored_message in array:
+                        self.on_receive(stored_message)
+        return
 
-    def post(self):
-        print('on receive')
+    def post(self, message):
+        try:
+            return self.message_queue.put_nowait(message)
+        except queue.Full:
+            print('I dont know what to do but the queue is full')
 
-    def schedule(self):
-        print('schedule')
-
-    def do_lookup(self):
-        print('lookup')
+    def do_lookup(self, name):
+        return self.actor_lookup[name]
 
     def shutdown(self):
-        print('shutdown')
+        self.is_running = False
+        self.on_shutdown()
 
     def set_complete(self):
-        print('set complete')
+        self.is_complete = True
--- micro_kernel.py
+++ micro_kernel.py
@@ -6,30 +6,51 @@ import micro_kernel_config
 class MicroKernel(object):
 
     def __init__(self):
+        self.micro_kernel_config = micro_kernel_config.MicroKernelConfig()
+
         self.is_running = True
         self.can_submit = True
         self.actor_lookup = dict()
         self.actor_future = dict()
-        self.timer = 'timer'
-
-        self.micro_kernel_config = micro_kernel_config.MicroKernelConfig()
-        self.pool = concurrent.futures.Executor()
-        self.monitor_future = 'something else'
+        self.pool = concurrent.futures.ThreadPoolExecutor(self.micro_kernel_config.MAX_THREADS)
+        self.monitor_future = concurrent.futures.Future()
 
     def submit(self, actor_instance):
         if self.can_submit:
             name = actor_instance.get_name()
             self.actor_lookup[name] = actor_instance
-            actor_instance.__init__(self.actor_lookup, self.micro_kernel_config, self.timer)
+            actor_instance.__init__(self.actor_lookup, self.micro_kernel_config)
             actor_instance.on_init()
-            future = self.pool.submit(actor_instance)
-            self.actor_future[name] = future
+            self.actor_future[name] = self.pool.submit(actor_instance)
             return True
         else:
             return False
 
     def start(self):
-        self.pool = concurrent.futures.ThreadPoolExecutor(self.micro_kernel_config.MAX_THREADS)
+        monitor = callable()
+
+        #@Override
+        def call():
+
+            while self.is_running or bool(self.actor_future):
+                for key, value in self.actor_future.items():
+                    future = value
+                    is_remove = False
+                    try:
+                        obj = future.result()
+                        is_remove = True
+                    except TimeoutError:
+                        pass
+                    if is_remove:
+                        value.on_complete()
+
+            return None
+
+        self.monitor_future = self.pool.submit(monitor)
 
     def shutdown(self):
-        print('microkernel shut down')
+        self.can_submit = False
+        self.is_running = False
+        for entry in self.actor_lookup.values():
+            entry.shutdown()
+        self.pool.shutdown()

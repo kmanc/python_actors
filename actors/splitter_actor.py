440414e4a745142c5e13c6df8c9c1193cbf4ff64
Kevin <kmanc@comcast.net>
2018-04-09 22:53:17 -0700

    splitter actor implemented

--- actors/splitter_actor.py
+++ actors/splitter_actor.py
@@ -1,15 +1,21 @@
-from actor import Actor
+from actor import Actor, DoneMessage
 
 
 class SplitterActor(Actor):
 
-    def __init__(self):
+    def __init__(self, key_list):
         super().__init__()
+        self.key_list = key_list
 
-    def on_receive(self, message):
-        # DOESNT WORK YET
-        print(message)
-
-    @classmethod
-    def do_work(cls, message):
-        pass
+    def on_receive(self, message_list):
+        while True:
+            for key in self.key_list:
+                try:
+                    instance = self.do_lookup(key)
+                    instance.post(message_list.pop(0))
+                except IndexError:
+                    for needs_shutdown in self.key_list:
+                        shut_me_down = self.do_lookup(needs_shutdown)
+                        shut_me_down.post(DoneMessage())
+                    self.is_complete = True
+                    return

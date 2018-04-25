cd212c51ea018ad42eb25fbcd8a0c650b001b7dc
Kevin <kmanc@comcast.net>
2018-04-24 17:58:41 -0700

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name="pythonactors",
-    version="0.0.1",
+    version="0.0.2",
     author="Kevin Conley",
     author_email="kmanc@comcast.net",
     description="Actor implementation in Python",
